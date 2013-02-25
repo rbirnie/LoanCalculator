4d0bbd0b6bacd2177357d3d2a2c032b3bd5aba57
rbirnie <rbirnie@gmail.com>
2013-02-25 14:51:07 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Loancost::Application.config.secret_token = '2bf72c7df3beb546bf1f4fec053d75806100ecc7f0af69396f0b2ec7df1f35ddfe14cf9fb9c6e2cc148c3bb5332f9c99cfee9a2f1d46a3819c26f264ce26ba0a'
