6ac6490bab6701aeb56c9bd247a23d41875d38c9
rbirnie <rbirnie@gmail.com>
2013-02-26 22:56:31 -0800

    deployment configs

---
+++ config/unicorn.rb
@@ -0,0 +1,9 @@
+root = "/home/robert/apps/LoanCalculator/current"
+working_directory root
+pid "#{root}/tmp/pids/unicorn.pid"
+stderr_path "#{root}/log/unicorn.log"
+stdout_path "#{root}/log/unicorn.log"
+
+listen "/tmp/unicorn.blog.sock"
+worker_processes 1
+timeout 30
